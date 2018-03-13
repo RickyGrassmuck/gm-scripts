92e04a9a11e019689955c674269877275c88050a
Ricky Grassmuck <rigrassm@gmail.com>
2018-03-13 18:15:55 -0500

    Initial Commit
    Signed-off-by: Ricky Grassmuck <rigrassm@gmail.com>

---
+++ reddit-tweaks.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name Reddit Hotkeys
+// @include https://www.reddit.com/*
+// @include https://reddit.com/*
+// @include https://*.reddit.com/*
+// @grant none
+// ==/UserScript==
+
+
+// Toggle sidebar, requires shine extension
+
+function toggle(className) {
+    $('html').toggleClass(className);
+}
+
+$('body').keyup(function(e) {
+  if (e.target.nodeName.toLowerCase() !== 'input' && e.target.nodeName.toLowerCase() !== 'textarea') {
+    e.preventDefault();
+    if(e.ctrlKey && e.altKey) {
+      switch( e.keyCode ) {
+		    case 77:
+          toggle("show-multireddits");
+          break;
+        case 83:
+          e.preventDefault();
+          toggle("show-sidebar");
+          break;
+      }
+    }
+  }
+});
